b4ed843cf2eb9be1a278589bae552347ae9567c6
Jonathan Freed <jon.freed@gmail.com>
2018-11-13 18:34:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,15 @@
 // add solution here
-// musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-// instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-//
-// function theBeatlesPlay(musicians, instruments) {
-//   var beatlesPlay = [];
-//
-//   for (var i = 0; i < musicians.length; i++) {
-//     beatlesPlay.push(musicians[i] + " plays " + instruments[i]
-// }
-// return beatlesPlay;
-// }
-//
-// musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-// instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
+  var beatlesPlay = [];
 
-     var allMusicians = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesPlay.push(musicians[i] + " plays " + instruments[i]
+}
+return beatlesPlay;
+}
 
-     for (var i=0; i < musicians.length; i++) {
-         allMusicians.push(musicians[i]  + " plays " + instruments[i]);
-     }
-     return allMusicians;
- }
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
