e289d6bc1df0115597e611474a2ac118ecdb6820
Jonathan Freed <jon.freed@gmail.com>
2018-11-13 18:26:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // add solution here
+musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay(musicians, instruments) {
   var theBeatlesPlay = [];
-  musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-  instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
   for (var i = 0; i < musicians.length; i++) {
     theBeatlesPlay.push(musicians[i] + " plays" + instruments[i] + "."
   }
