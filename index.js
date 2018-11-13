ee1b9c8a9e3903ad06adb87d9786849de566f4ed
Jonathan Freed <jon.freed@gmail.com>
2018-11-13 18:25:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments) {
   musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   for (var i = 0; i < musicians.length; i++) {
-    theBeatlesPlay.push(musicians[i] + " plays" + instruments[i] + "."
+    theBeatlesPlay.push(musicians[i] + " plays" + instruments[i] + ".";
   }
   return theBeatlesPlay;
 }
